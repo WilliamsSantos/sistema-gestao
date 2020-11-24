909856c2d82cfa6b486a7cac2713933a3c24016e
Williams S <31832571+WilliamsSantos@users.noreply.github.com>
2020-11-24 15:53:01 -0300

    Remove api prefix

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('api')->group(function () {
-    Route::post('/register', 'RegisterController@createUser')->name('create');
-    Route::get('/users', 'RegisterController@listAllRegistries')->name('get-users');
-    Route::put('/validate/user/{id}', 'ValidateController@validateUser')->name('validate-user');
-});
+Route::post('/register', 'RegisterController@createUser')->name('create');
+Route::get('/users', 'RegisterController@listAllRegistries')->name('get-users');
+Route::put('/validate/user/{id}', 'ValidateController@validateUser')->name('validate-user');
