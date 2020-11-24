b0aa3bbffc35f630f45d128b7c92aedd6e259821
Williams S <31832571+WilliamsSantos@users.noreply.github.com>
2020-11-24 15:52:10 -0300

    Update Web.php

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,3 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/{name}/registrar','RegisterController@render' )->name('register');
 Route::get('/{name}/validar', 'RegisterController@render')->middleware(['check.admin'])->name('valid');
-
-Route::prefix('api')->group(function () {
-    Route::post('/register', 'RegisterController@createUser')->name('create');
-    Route::get('/users', 'RegisterController@listAllRegistries')->name('get-users');
-    Route::put('/validate/user/{id}', 'ValidateController@validateUser')->name('validate-user');
-});
